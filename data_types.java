cf91bcb4948fc6a3baea67c441b8c51714f63208
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-11-03 06:25:35 -0700

    Add files via upload

---
+++ data_types.java
@@ -0,0 +1,28 @@
+public class data_types {
+    public static void main(String[] args) {
+       /* byte x=5;
+        int y=6;
+        short z=8;
+        //int a=y+z;
+        float b=6.54f+x;
+        System.out.println(b);
+         */
+
+        // Increments and Decrements opperators
+        int i=56;
+        //int b=i++;
+        int j=67;
+        int c=++j;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        int y=7;
+        System.out.println(++y *8);
+        char ch = 'a';
+        System.out.println(++ch);
+
+        
+    }
+    
+}
---
+++ operators.java
@@ -0,0 +1,27 @@
+public class operators {
+    public static void main(String[] args) {
+        // 1. Arithmetic Operators 
+        //int a =4;
+         //int b= 6 * a; // modulo operator
+         // 4.8%1.1 --> Returns Decimal Remainder
+        
+        // 2. Assigment Operators
+        //int b = 9;
+        //b *= 3;
+        //System.out.println(b);
+
+        // 3. comparison Operators
+        //System.out.println(64<6);
+
+        // 4. Logical operators
+        //System.out.println(64>5 || 64>98 );
+
+        // 5. Bitwise opperators
+        System.out.println(2&3);
+        //    10
+        //    11
+        //    ----
+        //    10
+
+    }
+}
