b307af159903f1599caff15c9c3c2629f468b2c6
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-11-16 05:19:06 -0800

    Add files via upload

---
+++ use_of_package.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; 
+//import java.util.*;
+
+public class use_of_package {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //java.util.Scanner sc = new java.util.scanner(System.in);
+        int a= sc.nextInt();
+        System.out.println("this is my scanner taking input as" +a);
+    }
+    
+}
