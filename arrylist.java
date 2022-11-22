445adb5c42cd8b30124945d4e6c441dd87f91940
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-11-22 04:12:30 -0800

    Add files via upload

---
+++ arrylist.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class arrylist {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1=new LinkedList<>();
+        LinkedList<Integer> l2=new LinkedList<>();
+        l2.add(15);
+        l2.add(18);
+        l2.add(19);
+
+        l1.add(6);
+        l1.add(7);
+        l1.add(4);
+        l1.add(6);
+        l1.add(0,5);
+        l1.add(0,1);
+        l1.add(6);
+
+
+    }
+    
+}
